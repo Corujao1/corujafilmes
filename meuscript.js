825b3e2c449ed32aef703b1819f02921d27404f7
Corujao1 <88009494+Corujao1@users.noreply.github.com>
2021-09-12 18:00:45 -0300

    Update meuscript.js

--- meuscript.js
+++ meuscript.js
@@ -1,3 +1,15 @@
 function descubra1() {
   document.getElementById("descubrir1").innerHTML = "Nota da ...";
 }
+function descubra2() {
+  document.getElementById("descubrir2").innerHTML = "Nota da ...";
+}
+function descubra3() {
+  document.getElementById("descubrir3").innerHTML = "Nota da ...";
+}
+function descubra4() {
+  document.getElementById("descubrir4").innerHTML = "Nota da ...";
+}
+function descubra5() {
+  document.getElementById("descubrir5").innerHTML = "Nota da ...";
+}
