92e0961204be0e9fd8e581a869687919a43baa50
Corujao1 <88009494+Corujao1@users.noreply.github.com>
2021-09-12 18:54:00 -0300

    Update meuscript.js

--- meuscript.js
+++ meuscript.js
@@ -1,15 +1,15 @@
 function descubra1() {
-  document.getElementById("descubrir1").innerHTML = "Nota da ...";
+  document.getElementById("descubrir1").innerHTML = "Nota Rotten Tomatoes:98%, Nota do IMDB:80%, Nota Metacritc:71%";
 }
 function descubra2() {
-  document.getElementById("descubrir2").innerHTML = "Nota da ...";
+  document.getElementById("descubrir2").innerHTML = "Nota Rotten Tomatoes:84%, Nota do IMDB:73%, Nota Metacritc:69%";
 }
 function descubra3() {
-  document.getElementById("descubrir3").innerHTML = "Nota da ...";
+  document.getElementById("descubrir3").innerHTML = "Nota Rotten Tomatoes:81%, Nota do IMDB:78%, Nota Metacritc:94%";
 }
 function descubra4() {
-  document.getElementById("descubrir4").innerHTML = "Nota da ...";
+  document.getElementById("descubrir4").innerHTML = "Nota Rotten Tomatoes:76%, Nota do IMDB:74%, Nota Metacritc:69%";
 }
 function descubra5() {
-  document.getElementById("descubrir5").innerHTML = "Nota da ...";
+  document.getElementById("descubrir5").innerHTML = "Nota Rotten Tomatoes:56%, Nota do IMDB:68%, Nota Metacritc:81%";
 }
